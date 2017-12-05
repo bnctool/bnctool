eac8ae6aba44419ff9bd75a42c6b5ef7a1f655c4
wuxuefeng <wuxuefeng@baidu.com>
2017-12-05 10:37:29 +0800

    注入

---
+++ static/bnc-mod.js
@@ -0,0 +1,226 @@
+/**
+ * Created by wuxuefeng on 2017/12/4.
+ */
+var bnc_require, bnc_define;
+
+(function(global) {
+    if (bnc_require) return; // 避免重复加载而导致已定义模块丢失
+
+    var head = document.getElementsByTagName('head')[0],
+        loadingMap = {},
+        factoryMap = {},
+        modulesMap = {},
+        scriptsMap = {},
+        resMap = {},
+        pkgMap = {};
+
+    function createScript(url, onerror) {
+        if (url in scriptsMap) return;
+        scriptsMap[url] = true;
+
+        var script = document.createElement('script');
+        if (onerror) {
+            var tid = setTimeout(onerror, bnc_require.timeout);
+
+            script.onerror = function() {
+                clearTimeout(tid);
+                onerror();
+            };
+
+            function onload() {
+                clearTimeout(tid);
+            }
+
+            if ('onload' in script) {
+                script.onload = onload;
+            } else {
+                script.onreadystatechange = function() {
+                    if (this.readyState == 'loaded' || this.readyState == 'complete') {
+                        onload();
+                    }
+                }
+            }
+        }
+        script.type = 'text/javascript';
+        script.src = url;
+        head.appendChild(script);
+        return script;
+    }
+
+    function loadScript(id, callback, onerror) {
+        var queue = loadingMap[id] || (loadingMap[id] = []);
+        queue.push(callback);
+
+        //
+        // resource map query
+        //
+        var res = resMap[id] || resMap[id + '.js'] || {};
+        var pkg = res.pkg;
+        var url;
+
+        if (pkg) {
+            url = pkgMap[pkg].url;
+        } else {
+            url = res.url || id;
+        }
+
+        createScript(url, onerror && function() {
+                onerror(id);
+            });
+    }
+
+    bnc_define = function(id, factory) {
+        id = id.replace(/\.js$/i, '');
+        factoryMap[id] = factory;
+
+        var queue = loadingMap[id];
+        if (queue) {
+            for (var i = 0, n = queue.length; i < n; i++) {
+                queue[i]();
+            }
+            delete loadingMap[id];
+        }
+    };
+
+    bnc_require = function(id) {
+
+        // compatible with require([dep, dep2...]) syntax.
+        if (id && id.splice) {
+            return bnc_require.async.apply(this, arguments);
+        }
+
+        id = bnc_require.alias(id);
+
+        var mod = modulesMap[id];
+        if (mod) {
+            return mod.exports;
+        }
+
+        //
+        // init module
+        //
+        var factory = factoryMap[id];
+        if (!factory) {
+            throw '[BNC_COMMONJS] Cannot find module `' + id + '`';
+        }
+
+        mod = modulesMap[id] = {
+            exports: {}
+        };
+
+        //
+        // factory: function OR value
+        //
+        var ret = (typeof factory == 'function') ? factory.apply(mod, [bnc_require, mod.exports, mod]) : factory;
+
+        if (ret) {
+            mod.exports = ret;
+        }
+        return mod.exports;
+    };
+
+    bnc_require.async = function(names, onload, onerror) {
+        if (typeof names == 'string') {
+            names = [names];
+        }
+
+        var needMap = {};
+        var needNum = 0;
+
+        function findNeed(depArr) {
+            for (var i = 0, n = depArr.length; i < n; i++) {
+                //
+                // skip loading or loaded
+                //
+                var dep = bnc_require.alias(depArr[i]);
+
+                if (dep in factoryMap) {
+                    // check whether loaded resource's deps is loaded or not
+                    var child = resMap[dep] || resMap[dep + '.js'];
+                    if (child && 'deps' in child) {
+                        findNeed(child.deps);
+                    }
+                    continue;
+                }
+
+                if (dep in needMap) {
+                    continue;
+                }
+
+                needMap[dep] = true;
+                needNum++;
+                loadScript(dep, updateNeed, onerror);
+
+                var child = resMap[dep] || resMap[dep + '.js'];
+                if (child && 'deps' in child) {
+                    findNeed(child.deps);
+                }
+            }
+        }
+
+        function updateNeed() {
+            if (0 == needNum--) {
+                var args = [];
+                for (var i = 0, n = names.length; i < n; i++) {
+                    args[i] = bnc_require(names[i]);
+                }
+
+                onload && onload.apply(global, args);
+            }
+        }
+
+        findNeed(names);
+        updateNeed();
+    };
+
+    bnc_require.resourceMap = function(obj) {
+        var k, col;
+
+        // merge `res` & `pkg` fields
+        col = obj.res;
+        for (k in col) {
+            if (col.hasOwnProperty(k)) {
+                resMap[k] = col[k];
+            }
+        }
+
+        col = obj.pkg;
+        for (k in col) {
+            if (col.hasOwnProperty(k)) {
+                pkgMap[k] = col[k];
+            }
+        }
+    };
+
+    bnc_require.loadJs = function(url) {
+        createScript(url);
+    };
+
+    bnc_require.loadCss = function(cfg) {
+        if (cfg.content) {
+            var sty = document.createElement('style');
+            sty.type = 'text/css';
+
+            if (sty.styleSheet) { // IE
+                sty.styleSheet.cssText = cfg.content;
+            } else {
+                sty.innerHTML = cfg.content;
+            }
+            head.appendChild(sty);
+        } else if (cfg.url) {
+            var link = document.createElement('link');
+            link.href = cfg.url;
+            link.rel = 'stylesheet';
+            link.type = 'text/css';
+            head.appendChild(link);
+        }
+    };
+
+
+    bnc_require.alias = function(id) {
+        return id.replace(/\.js$/i, '');
+    };
+
+    bnc_require.timeout = 5000;
+
+})(this);
